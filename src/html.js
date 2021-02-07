4ea35f631e2ab16fb9eff35d9aefeb22a62f813b
Austin Lopez-Gomez <aflopez@amazon.com>
2021-02-07 10:49:08 -0500

    add html

--- src/html.js
+++ src/html.js
@@ -5,31 +5,31 @@ export default function HTML(props) {
   return (
     <html {...props.htmlAttributes}>
       <head>
-        <link rel="stylesheet" href="https://use.typekit.net/hhv4wba.css">
-          <meta charSet="utf-8" />
-          <meta httpEquiv="x-ua-compatible" content="ie=edge" />
-          <meta
-            name="viewport"
-            content="width=device-width, initial-scale=1, shrink-to-fit=no"
-          />
-          {props.headComponents}
+        <link rel="stylesheet" href="https://use.typekit.net/hhv4wba.css" />
+        <meta charSet="utf-8" />
+        <meta httpEquiv="x-ua-compatible" content="ie=edge" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, shrink-to-fit=no"
+        />
+        {props.headComponents}
 
       </head>
-        <body {...props.bodyAttributes}>
-          {props.preBodyComponents}
-          <div
-            key={`body`}
-            id="___gatsby"
-            dangerouslySetInnerHTML={{ __html: props.body }}
-          />
-          {props.postBodyComponents}
-        </body>
+      <body {...props.bodyAttributes}>
+        {props.preBodyComponents}
+        <div
+          key={`body`}
+          id="___gatsby"
+          dangerouslySetInnerHTML={{ __html: props.body }}
+        />
+        {props.postBodyComponents}
+      </body>
     </html>
   )
 }
 
 HTML.propTypes = {
-        htmlAttributes: PropTypes.object,
+  htmlAttributes: PropTypes.object,
   headComponents: PropTypes.array,
   bodyAttributes: PropTypes.object,
   preBodyComponents: PropTypes.array,
